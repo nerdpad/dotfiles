8fed4dcdf142caff735788f6dac9a7a5560f312d
nerdpad <zuhaib@nerdpad.com>
2017-06-11 16:24:13 -0400

    add alias for tmux

--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -3,6 +3,7 @@ alias ta='tmux attach'
 alias tls='tmux ls'
 alias tat='tmux attach -t'
 alias tns='tmux new-session -s'
+alias tnsd='tmux new-session -s dev'
 
 # mimic vim functions
 alias :q='exit'
