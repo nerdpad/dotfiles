3b07fc0c92a70647315c8848bf1f8366dbe1895f
Zuhaib <zuhaib@nerdpad.com>
2022-01-25 16:55:15 -0500

    add antibody and fix kitty font

--- Brewfile
+++ Brewfile
@@ -50,3 +50,4 @@ brew "wdiff" # word differences in text files
 brew "wget" # internet file retriever
 brew "z" # switch between most used directories
 brew "zsh" # zsh (latest)
+brew "antibody" # shell plugin manager
---
+++ zsh/antibody.zsh
@@ -0,0 +1,16 @@
+source <(antibody init)
+
+antibody bundle "
+
+zsh-users/zsh-completions
+zsh-users/zsh-autosuggestions
+
+zsh-users/zsh-syntax-highlighting
+zsh-users/zsh-history-substring-search
+
+robbyrussell/oh-my-zsh kind:zsh path:lib/git.zsh 
+robbyrussell/oh-my-zsh path:plugins/git
+robbyrussell/oh-my-zsh path:plugins/ssh-agent
+robbyrussell/oh-my-zsh path:plugins/vi-mode
+
+"
