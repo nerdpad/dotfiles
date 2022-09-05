c94e12ca766cd51d43e2bfe2f8085c844a13405a
Nick Nisi <nick@nisi.org>
2022-09-05 07:02:59 -0500

    feat(vim): add rust lsp config

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -217,6 +217,14 @@ lsp_installer.setup(
   }
 )
 
+lspconfig.rust_analyzer.setup(
+  make_config(
+    function(config)
+      return config
+    end
+  )
+)
+
 lspconfig.tsserver.setup(
   make_config(
     function(config)
@@ -238,6 +246,7 @@ lspconfig.tsserver.setup(
 lspconfig.denols.setup(
   make_config(
     function(config)
+      config.single_file_support = false
       config.root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc")
       config.init_options = {
         lint = true
